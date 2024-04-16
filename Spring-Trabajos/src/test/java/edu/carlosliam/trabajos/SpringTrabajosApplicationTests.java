47ce3e402a47bd2aba36ec6fc7153c55b8162ff2
Muyiam <liam.alejoasensi@gmail.com>
2024-04-16 15:31:44 +0200

    Estructura inicial del proyecto

---
+++ Spring-Trabajos/src/test/java/edu/carlosliam/trabajos/SpringTrabajosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carlosliam.trabajos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrabajosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
