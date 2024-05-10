254f7a5b9efe66e628a15ce4585d0a8971932fa9
carlosriverogv <karlos.riverog@gmail.com>
2024-05-10 12:40:12 +0200

    Modified TrabajoRestController to HotelManagement

--- Spring-Trabajos/src/main/java/edu/carlosliam/trabajos/controllers/TrabajoRestController.java
+++ Spring-Trabajos/src/main/java/edu/carlosliam/trabajos/controllers/TrabajoRestController.java
@@ -1,10 +1,15 @@
 package edu.carlosliam.trabajos.controllers;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,20 +21,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.carlosliam.trabajos.models.entity.Trabajador;
 import edu.carlosliam.trabajos.models.entity.Trabajo;
 import edu.carlosliam.trabajos.models.services.ITrabajoService;
 
 @CrossOrigin(origins = {"*"})
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", produces = "application/json; charset=UTF-8")
 public class TrabajoRestController {
 	
 	@Autowired
 	private ITrabajoService trabajoService;
 	
-	@GetMapping("/trabajos")
+	// Crear respuestas de error
+	private ResponseEntity<?> createErrorResponse(HttpStatus status, String errorMessage) {
+	    Map<String, Object> response = new HashMap<>();
+	    
+	    response.put("error", true);
+	    response.put("errorMessage", errorMessage);
+	    
+	    return new ResponseEntity<>(response, status);
+	}
+	
+	// Crear respuestas correctas
+	private ResponseEntity<?> createResultResponse(HttpStatus status, Object result) {
+		Map<String, Object> response = new HashMap<>();
+		
+		response.put("error", false);
+		response.put("result", result);
+		
+		return new ResponseEntity<>(response, status);
+	}
+	
+	/*@GetMapping("/trabajos")
 	public List<Trabajo> index() {
 		return trabajoService.findAll();
+	}*/
+	
+	@GetMapping("/trabajos")
+	public ResponseEntity<?> index() {
+		List<Trabajo> trabajo = new ArrayList<>();
+		
+		try {
+			trabajo.addAll(this.trabajoService.findAll());
+		} catch(DataAccessException e) {
+			return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, 
+					"Error al realizar la consulta en la base de datos.");
+		}
+		
+		return createResultResponse(HttpStatus.OK, trabajo);
 	}
 	
 	@GetMapping("/trabajos/{id}")
